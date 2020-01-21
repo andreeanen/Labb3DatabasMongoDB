7cb0ddf46fb385704bbf6b837226ca49be5886dd
Andreea Nenciu <andreeanen@yahoo.com>
2020-01-21 12:12:42 +0100

    Change id to ObjectId, Print restaurants, Filter Cafe

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
+using System;
 
 namespace Labb3
 {
@@ -10,47 +11,75 @@ namespace Labb3
             MongoClient client = new MongoClient("mongodb://localhost:27017");
             var database = client.GetDatabase("lab3");
             database.DropCollection("restaurants");
-            var collection = database.GetCollection<BsonDocument>("restaurants");
+            var restaurantsCollection = database.GetCollection<BsonDocument>("restaurants");
 
-            BsonDocument[] documents = new BsonDocument[]
+            //Create bson array 
+            BsonDocument[] restaurants = new BsonDocument[]
             {
                 new BsonDocument
                 {
-                    { "_id",1},
+                    { "_id",ObjectId.Parse("5c39f9b5df831369c19b6bca")},
                     { "name" , "Sun Bakery Trattoria" },
                     { "stars" , 4},
                     { "categories" , new BsonArray {"Pizza", "Pasta", "Italian", "Coffee", "Sandwiches" } }
                 },
                 new BsonDocument
                 {
-                    { "_id", 2},
+                    { "_id", ObjectId.Parse("5c39f9b5df831369c19b6bcb")},
                     { "name" , "Blue Bagels Grill" },
                     { "stars" , 3 },
                     { "categories" , new BsonArray {"Bagels", "Cookies", "Sandwiches" } }
                 },
                 new BsonDocument
                 {
-                    { "_id" , 3 },
+                    { "_id" , ObjectId.Parse("5c39f9b5df831369c19b6bcc") },
                     { "name","Hot Bakery Cafe" },
                     { "stars" , 4},
                     { "categories" , new BsonArray {"Bakery","Cafe","Coffee","Dessert" } }
                 },
                 new BsonDocument
                 {
-                    { "_id" , 4 },
+                    { "_id" , ObjectId.Parse("5c39f9b5df831369c19b6bcd") },
                     { "name" , "XYZ Coffee Bar"},
                     { "stars" , 5 },
                     { "categories" , new BsonArray {"Coffee", "Cafe", "Bakery", "Chocolates" } }
                 },
                 new BsonDocument
                 {
-                    { "_id" , 5 },
+                    { "_id" , ObjectId.Parse("5c39f9b5df831369c19b6bce") },
                     { "name" , "456 Cookies Shop" },
                     { "stars" , 4},
                     {"categories", new BsonArray {"Bakery", "Cookies", "Cake", "Coffee"} }
                 }
             };
-            collection.InsertMany(documents);
+            restaurantsCollection.InsertMany(restaurants);
+
+            //Print out the restaurants
+            //var restaurantsFind = restaurantsCollection.Find(new BsonDocument());
+            //foreach (var restaurant in restaurantsFind.ToEnumerable())
+            //{
+            //    Console.WriteLine(restaurant.ToString()); //or
+            //    //Console.WriteLine(restaurant.ToJson());
+            //}
+
+            //Filter restaurants that hace "Cafe" among its categories
+            var filterCafe = Builders<BsonDocument>.Filter.Eq("categories", "Cafe");
+            var projectionCafe = Builders<BsonDocument>.Projection.Include("name").Exclude("_id");
+            var resultCafe = restaurantsCollection.Find<BsonDocument>(filterCafe).Project(projectionCafe).ToList();
+            foreach (var restaurant in resultCafe)
+            {
+                Console.WriteLine(restaurant.ToString());
+            }
+
+
+
+
+
+
+
+
+
+            Console.ReadKey(true);
         }
     }
 }
